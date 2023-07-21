7e2f9081711a5fa9e746cbccfad77b71e5f8aaef
yutaro0602 <nakamuray@scala-partners.com>
2023-07-21 10:58:35 +0900

    コンポーネントツリー完成

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -4,13 +4,18 @@ const Star = ({ selected = false, onSelect = f => f}) => (
   <FaStar color={selected ? "red" : "grey" } onClick={onSelect} />
 );
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({ 
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = f => f
+}) {
   return (
     <div style={{ padding: "5px" }} >
     {[...Array(totalStars)].map((n,i) => (
       <Star 
         key={i} 
         selected={selectedStars > i} 
+        onSelect={() => onRate(i + 1)}
       />
     ))}
     <p>{selectedStars} of {totalStars} stars</p>
